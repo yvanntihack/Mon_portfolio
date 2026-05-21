0e01b1b79dc4fd4306ec2e47358b329ebb5ebcf1
ezechiel_dev225 <ezekielkoume@hotmail.fr>
2026-05-21 02:17:08 +0000

    modification index.ts

--- server/index.ts
+++ server/index.ts
@@ -18,10 +18,10 @@ async function startServer() {
     res.sendFile(path.join(staticPath, "index.html"));
   });
 
-  const port = process.env.PORT || 3000;
+  const port = parseInt(process.env.PORT || "3000", 10);
 
-  server.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}/`);
+  server.listen(port, "0.0.0.0", () => {
+    console.log(`Server running on port ${port}`);
   });
 }
 
